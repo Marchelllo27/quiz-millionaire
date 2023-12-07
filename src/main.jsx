8dcb7587557788893c4303200747532cf54c7ad1
Marc Tofan <tofmana@gmail.com>
2023-12-06 22:12:55 -0800

    before changes answer's styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App.jsx";
@@ -6,11 +7,11 @@ import "./index.css";
 import MusicContextProvider from "./store/MusicContextProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // <React.StrictMode>
-  <GameContextProvider>
-    <MusicContextProvider>
-      <App />
-    </MusicContextProvider>
-  </GameContextProvider>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <GameContextProvider>
+      <MusicContextProvider>
+        <App />
+      </MusicContextProvider>
+    </GameContextProvider>
+  </React.StrictMode>
 );
